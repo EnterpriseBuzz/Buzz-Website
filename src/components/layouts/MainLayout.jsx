611148283ebb88261de43a4c6ebd05fe9a93522a
shieldstring <edmunddaniels06@gmail.com>
2025-04-05 23:31:54 +0100

    updated HDEW

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
+import { useEffect } from "react";
 
 export default function MainLayout() {
+  const location = useLocation();
+  const MEASUREMENT_ID = "G-71LW17H5QC"; // Replace with your actual GA4 Measurement ID
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", MEASUREMENT_ID, {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location, MEASUREMENT_ID]);
   return (
     <>
       <Header />
