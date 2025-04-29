66263015f806cb708dc70185c086337db35aaf0b
shieldstring <edmunddaniels06@gmail.com>
2025-04-29 08:32:27 +0100

    updated pricing 3xl screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('tailwind-scrollbar-hide')],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "1920px", // Example: Adjust the pixel value as needed for your 3xl breakpoint
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
 };
